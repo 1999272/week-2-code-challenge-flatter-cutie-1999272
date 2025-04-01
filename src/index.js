6a3d08bca4b1f8bac1835a892ddef00d329a2272
Mark_Kamau <mark.wainaina@student.moringaschool.com>
2025-04-01 14:47:10 +0300

    Completed setting up DOM elements and API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-// Your code here
+// Base URL for the API
+const baseURL = "http://localhost:3000";
+
+// DOM Elements
+const characterBar = document.getElementById("character-bar");
+const detailedInfo = document.getElementById("detailed-info");
+const votesForm = document.getElementById("votes-form");
+
+
+
